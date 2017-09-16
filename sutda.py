44e0c25474f8ea109ca09d24848508987f16a976
ryoogh96 <31858004+ryoogh96@users.noreply.github.com>
2017-09-16 17:12:00 +0900

    Delete Secondp1,p2

--- sutda.py
+++ sutda.py
@@ -4,35 +4,33 @@ import sys
 global lst
 global p1
 global p2
-global Secondp1
-global Secondp2
 global pot
 global turnPlayer
 global notTurnPlayer
 global cur_bet
 global f
+global f1
+global f2
+global data
+global data1
+global data2
+data=""
+data1=""
+data2=""
 global p1stack
 global p2stack
-global Secondp1stack
-global Secondp2stack
 p1stack = 0
 p2stack = 0
-Secondp1stack = 0
-Secondp2stack = 0
 global betcnt
 betcnt = 0
 global setcost
 setcost = 10
-global IsCall
-IsCall = 0
 global showJokbo
 global showJokbo2
-showJokbo = ""
+showJokbo = ""	
 showJokbo2 = ""
 p1 = []
 p2 = []
-Secondp1 = []
-Secondp2 = []
 p1.append(1000)
 p2.append(1000)
 pot = 0
@@ -41,27 +39,23 @@ notTurnPlayer = p2
 
 lst = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
+def Set(firstCard, secondCard) :
+	Jokbo = {'38GwangDDeng' : 100,
+			'GwangDDeng' : 95,
+			'DDeng' : 90,
+			'ArLi' : 85,
+			'Doksa' : 80,
+			'9BBing' : 75,
+			'JangBBing' : 70,
+			'Sseryuk' : 65,
+			'Gab5' : 60
+			}	
+
+Jokbo.get('38GwangDDeng')		
 
 
-"'class familytree:                                                       # Lists of Jokbo                                                     
-        Sol = [1,1]
-        Maejo = [2,2]
-        Sakura = [3,3]
-        Heukssari = [4,4]
-        Cho = [5,5]
-        Mokdan = [6,6]
-        Hongssari = [7,7]
-        Gongsan = [8,8]
-        Gukjin = [9,9]
-        Pung = [10,10]
-"' 	
-
-def Jokbo(firstCard,secondCard):
-	for i in range(20)
-		
-	
-        
-def playerChange():
+														
+def playerChange():											
 	global turnPlayer
 	if(turnPlayer == p1):
 		turnPlayer = p2
@@ -78,27 +72,35 @@ def fight():
 	global pot
 	global p1stack
 	global p2stack
-	global IsCall
 	global showJokbo
-	
-	if(IsCall = 1):
-		showJokbo = Jokbo(p1,Secondp1)
-		showJokbo2 = Jokbo(p2, Secondp2)
-		
-		
-			if(value(p1)>value(p2)):
-				print("p1 wins")
-				p1[0] += pot
-				pot = 0
-				p1stack = 0
-				p2stack = 0
+		showJokbo = Jokbo(p1[1],p1[2])
+		showJokbo2 = Jokbo(p2[1],p2[2] )
 
-			else:
-				print("p2 wins")
-				p2[0] += pot
-				pot = 0
-				p1stack = 0
-				p2stack = 0
+	global f1
+	global data1
+	global f2
+	global data2
+	if(value(p1)>value(p2)):
+
+		print("p1 wins")
+		f1.write(data1)
+		data1 = ""
+		data2 = ""
+		p1[0] += pot
+		pot = 0
+		p1stack = 0
+		p2stack = 0
+
+	elif(value(p2)>value(p1)):
+		
+		print("p2 wins")
+		f2.write(data2)
+		data1=""
+		data2=""
+		p2[0] += pot
+		pot = 0
+		p1stack = 0
+		p2stack = 0
 
 	resetGame()
 	singleGame()
@@ -123,10 +125,6 @@ def resetGame():
 	lst.append(p1.pop())
 	lst.append(p2.pop())
 	lst.append(p2.pop())
-	lst.append(Secondp1.pop())
-	lst.append(Secondp1.pop())
-	lst.append(Secondp2.pop())
-	lst.append(Secondp2.pop())	
 
 def bet(player):
 	global pot
@@ -139,31 +137,35 @@ def bet(player):
 		global betcnt,p1stack,p2stack,betMoney
 		
 		if(turnPlayer == p1):
-			# data = str(id(p1))+","
-			data = str(p1[1])+","
-			data += str(p1[2])+","
-			data += str(p1[0])+","
-			data += str(p2[0])+","
-			data += str(p1stack)+","
-			data += str(p2stack)+","
-			data += str(betcnt)+","
+			global data1
+			# data1 = str(id(p1))+","
+			data1 = str(p1[1])+","
+			data1 += str(p1[2])+","
+			data1 += str(p1[0])+","
+			data1 += str(p2[0])+","
+			data1 += str(p1stack)+","
+			data1 += str(p2stack)+","
+			data1 += str(betcnt)+","
 			betMoney = random.randrange(cur_bet,turnPlayer[0])
 			p1stack += betMoney
+			betcnt = betcnt + 1
+			data1 += str(betMoney)+"\n"
 		else:
-			# data = str(id(p2))+","
-			data = str(p2[1])+","
-			data += str(p2[2])+","
-			data += str(p2[0])+","
-			data += str(p1[0])+","
-			data += str(p2stack)+","
-			data += str(p1stack)+","
-			data += str(betcnt)+","
+			global data2
+			# data2 = str(id(p2))+","
+			data2 = str(p2[1])+","
+			data2 += str(p2[2])+","
+			data2 += str(p2[0])+","
+			data2 += str(p1[0])+","
+			data2 += str(p2stack)+","
+			data2 += str(p1stack)+","
+			data2 += str(betcnt)+","
 			betMoney = random.randrange(cur_bet,turnPlayer[0])
 			p2stack += betMoney
-		betcnt = betcnt + 1
-		data += str(betMoney)+"\n"
-		f.write(data)
-		data = ""
+			
+			betcnt = betcnt + 1
+			data2 += str(betMoney)+"\n"
+		
 	else:
 		betMoney = int(input("bet money limited by "+str(player[0])+" previous betting was "+str(cur_bet)+"\n"))
 	if(betMoney<=player[0] and betMoney>=cur_bet and betMoney>0):
@@ -173,7 +175,7 @@ def bet(player):
 		turnPlayer[0] -= betMoney
 		pot += betMoney
 		if(cur_bet == betMoney):
-			Call()
+			print("CALL!")
 		else:
 			cur_bet = betMoney
 			return
@@ -184,15 +186,6 @@ def bet(player):
 			print("invalid input occured, input should be over "+str(cur_bet))
 			betMoney = 0
 			bet(player)
-def Call():
-        global IsCall
-        
-		print("Do you want to Call? Y or N")
-		if(sys.argv[1] == Y)
-			IsCall = 1										# Player has called
-			fight()
-			else
-				fight()
         
 def allIn():
 	global pot
@@ -227,8 +220,6 @@ def allIn():
 			pot += turnPlayer[0]
 			turnPlayer[0] = 0
 			betcnt = betcnt+1
-			f.write(data)
-			data = ""
 			fight()
 		else:
 			surrender()
@@ -262,15 +253,7 @@ def kiri(deck):
 	return deck
 
 def appending():
-	global IsCall
 	global turnPlayer
-	global Secondp1
-	if(IsCall == 1)												# if player has called draw one more card
-		for i in range(4):
-			Secondp1.append(lst.pop())
-			playerChange()
-			return
-			else
 				for i in range(4):
 					turnPlayer.append(lst.pop())
 					playerChange()
@@ -325,5 +308,9 @@ def singleGame():
 
 if(sys.argv[1] == 'test'):
 	global f
+	global f1
+	global f2
 	f = open('testdata.txt','a')
+	f1 = open('testdata1.txt','a')
+	f2 = open('testdata2.txt','a')
 singleGame()

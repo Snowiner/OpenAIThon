b92366d3af5c544a3fe8d5e6cb75597839684a5d
Snow <21000737@handong.edu>
2017-09-17 08:26:52 +0900

    원인불명 미작동

--- twoCardPoker_1.2.py
+++ twoCardPoker_1.2.py
@@ -7,11 +7,11 @@ class Game:
     def __init__(self, player1, player2):
         self.pot = 0
         self.lst = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-        self.players = [player1,player2]
-        self.turnPlayer = random.randrange(0,2)
+        self.turnPlayer = player1
+        self.nonPlayer = player2
         self.cur_bet = 0
         self.betcnt = 0
-        self.available = True
+        self.available = 0
 
     def cardAppending(self):
         self.lst = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
@@ -34,13 +34,18 @@ class Game:
         self.players[1].potMoney = 0
 
     def playerChange(self):
-        self.turnPlayer = (self.turnPlayer+1)%2
-
+        temp = id(self.turnPlayer)
+        self.turnPlayer = id(self.nonPlayer)
+        self.nonPlayer = temp
+        
     def input_user(self):
-        return self.input_validator(input("betting range : " + self.cur_bet + " ~ " + self.players[self.turnPlayer].money))
+        prevalue = input("betting range : " + self.cur_bet + " ~ " + self.players[self.turnPlayer].money)
+        value = self.input_validator(prevalue)
+        return value
 
     def input_random(self):
-        return self.input_validator(random.randrange(0,self.players[self.turnPlayer].money+1))
+        value = random.randrange(0,self.players[self.turnPlayer].money+1)
+        return self.input_validator(value)
 
     def input_AI(self):
         value = int(serve.betting(self.players[self.turnPlayer].cards[0], self.players[self.turnPlayer].cards[1], self.players[self.turnPlayer].money, self.players[(self.turnPlayer+1)%2].money, self.players[self.turnPlayer].potMoney, self.players[(self.turnPlayer+1)%2].potMoney, self.betcnt))
@@ -52,7 +57,7 @@ class Game:
         elif(value == 0):
             return 0
         elif(value < self.cur_bet):
-            if(random.randrange(0,self.cur_bet)>value):
+            if(random.randrange(0,self.players[self.turnPlayer].money+1)>value):
                 return 0
             else:
                 return self.cur_bet
@@ -76,10 +81,10 @@ class Game:
                 self.pot += self.players[self.turnPlayer].money
                 self.players[self.turnPlayer] = 0
                 self.fight()
-                return False
+                return 1
             else:
                 self.surrender()
-                return False
+                return 1
 
     def all_in_check_user(self):
         return input("True or False")
@@ -92,6 +97,8 @@ class Game:
 
 
     def betting(self):
+        bettingMoney = 0
+
         if(self.players[self.turnPlayer].type == 'user'):
             bettingMoney = self.input_user()
         elif(self.players[self.turnPlayer].type == 'AI'):
@@ -99,15 +106,16 @@ class Game:
         else:
             bettingMoney = self.input_random()
 
-        for i in [self.players[self.turnPlayer].cards[0], self.players[self.turnPlayer].cards[1], self.players[self.turnPlayer].money, self.players[(self.turnPlayer+1)%2].money, self.players[self.turnPlayer].potMoney, self.players[(self.turnPlayer+1)%2].potMoney]:
-            self.players[self.turnPlayer].data += str(i)
-            self.players[self.turnPlayer].data += ','
-        self.players[self.turnPlayer].data+= str(self.betcnt)
+        # print(bettingMoney)
+
+        # for i in [self.players[self.turnPlayer].cards[0], self.players[self.turnPlayer].cards[1], self.players[self.turnPlayer].money, self.players[(self.turnPlayer+1)%2].money, self.players[self.turnPlayer].potMoney, self.players[(self.turnPlayer+1)%2].potMoney]:
+        #     self.players[self.turnPlayer].data += str(i)
+        #     self.players[self.turnPlayer].data += ','
+        # self.players[self.turnPlayer].data+= str(self.betcnt)
 
         self.pot += bettingMoney
         self.players[self.turnPlayer].potMoney += bettingMoney
         if(bettingMoney == 0):
-            print(bettingMoney)
             self.surrender()
         elif(bettingMoney == self.cur_bet):
             self.fight()
@@ -120,14 +128,14 @@ class Game:
             players[0].fileStream.write(players[0].data)
             self.players[0].money += self.pot
             self.clearBoard()
-            self.available = False
+            self.available = 1
 
         elif (self.players[0].value() < self.players[1].value()):
             print("player2 wins")
             players[1].fileStream.write(players[1].data)
             self.players[1].money += self.pot
             self.clearBoard()
-            self.available = False
+            self.available = 1
 
         else:
             print("draw")
@@ -136,11 +144,12 @@ class Game:
         print("Folded by player"+str(self.turnPlayer+1))
         self.players[(self.turnPlayer+1)%2].money+= self.pot
         self.clearBoard()
-        self.available = False
+        self.available = 1
 
 class Player:
     def __init__(self,type,fileName):
         self.type = type
+        print(self.type)
         self.money = 1000
         self.cards = [0,0]
         self.potMoney = 0
@@ -149,17 +158,23 @@ class Player:
 
     def value(self):
         return (self.cards[0] + self.cards[1]) % 10
+
 def flow():
-    player1 = Player('random','DataOfLastDay1.csv')
-    player2 = Player('random','DataOfLastDay2.csv')
+    player1 = Player('user','DataOfLastDay1.csv')
+    print("player1 set")
+    player2 = Player('user','DataOfLastDay2.csv')
+    print("player2 set")
     game = Game(player1,player2)
-
+    print("game get")
     game.cardAppending()
-
+    print(player1.cards)
+    print(player2.cards)
+    print(game.players)
     game.preCheck()
 
-    while(game.available == True):
+    while(game.available != 1):
         game.betting()
-for i in range(0,100):
-    flow()
+
+# for i in range(0,100):
+flow()
 
